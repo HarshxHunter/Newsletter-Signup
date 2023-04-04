12b34222acbc39720bed76d0726546f632048390
Harsh Singh Kaira <kairaharsh793@gmail.com>
2023-04-04 20:11:23 +0530

    api key name change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser= require("body-parser");
 
 const app=express();
 const port = process.env.PORT || 3000;
-const apiKey= process.env.Chimp_Key;
+const apiKey= process.env.Chimp_key;
 
 app.use(bodyParser.urlencoded({extended:true}));
 
